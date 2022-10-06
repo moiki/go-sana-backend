cc45e9dbec218915209b4c59f6b7881fcad4f076
moisesRadix <mreyes@radixhaven.com>
2022-10-06 00:05:18 +0000

    implement express deploy

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const app = express();
+const noCache = require('nocache');
+const featurePolicy = require('feature-policy');
+require('dotenv').config();
+// var expressStaticGzip = require("compression");
+const expressStaticGzip = require('express-static-gzip');
+//
+// app.use(
+//     expressStaticGzip(path.join(__dirname, 'build'), {
+//         enableBrotli: false, // only if you have brotli files too
+//     })
+// );
+// app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(
+    featurePolicy({
+        features: {
+            fullscreen: ["'self'"],
+            camera: ["'self'"],
+            payment: ["'self'"],
+            microphone: ["'self'"],
+        },
+    })
+);
+
+app.use(noCache());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+http.createServer(app).listen(3000, () => {
+    console.log("Running frontend from express in http://localhost:3000...")
+});
