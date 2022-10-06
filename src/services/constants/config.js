2b81b3a461720b18b99825e87dac62008d81f52a
moisesRadix <mreyes@radixhaven.com>
2022-10-06 00:18:33 +0000

    fix config env vars

--- src/services/constants/config.js
+++ src/services/constants/config.js
@@ -1,7 +1,7 @@
 // import 'dotenv/config'
 
 export default {
-    rest_uri_prod: process.env.REACT_APP_REST_DEV_URL || import.meta.env.VITE_REACT_APP_REST_PROD_URL,
-    rest_uri_dev: process.env.REACT_APP_REST_DEV_URL || import.meta.env.VITE_REACT_APP_REST_PROD_URL,
-    node_env: process.env.NODE_ENV || import.meta.env.VITE_NODE_ENV
+    rest_uri_prod: process.env.REACT_APP_REST_DEV_URL,
+    rest_uri_dev: process.env.REACT_APP_REST_DEV_URL,
+    node_env: process.env.NODE_ENV
 }
